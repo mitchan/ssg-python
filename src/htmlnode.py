ed5287c264b1288951f03e889849a4a3461a9588
Francesco Casetta <francesco.casetta@gmail.com>
2025-03-16 17:57:18 +0100

    leaf node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -26,3 +26,15 @@ class HtmlNode:
         return (
             f"<{self.tag}{props if props == "" else " " + props}>{content}</{self.tag}>"
         )
+
+
+class LeafNode(HtmlNode):
+    def __init__(self, value, tag=None, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Value cannot be None")
+        if self.tag is None:
+            return self.value
+        return f"{self}"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HtmlNode
+from htmlnode import HtmlNode, LeafNode
 
 
 class TestHtmlNode(unittest.TestCase):
@@ -27,6 +27,20 @@ class TestHtmlNode(unittest.TestCase):
             f"{parent}",
             "<div><p>this is a paragraph</p><span>this is a span</span></div>",
         )
+
+
+class TestHtmlLeaf(unittest.TestCase):
+    def test_no_tag(self):
+        leaf = LeafNode("this is a text")
+        self.assertEqual("this is a text", leaf.to_html())
+
+    def test_paragraph(self):
+        leaf = LeafNode(value="this is a paragraph", tag="p", props={"class": "mb-4"})
+        self.assertEqual('<p class="mb-4">this is a paragraph</p>', leaf.to_html())
+
+    def test_no_value_exception(self):
+        leaf = LeafNode(value=None)
+        self.assertRaises(ValueError, leaf.to_html)
 
 
 if __name__ == "__main__":
