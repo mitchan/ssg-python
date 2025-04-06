6ab162c864ed28b824d4bb68817cd784044c05ac
Francesco Casetta <francesco.casetta@gmail.com>
2025-04-06 12:02:28 +0200

    feat: extract links

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,13 +1,13 @@
 import unittest
 
+import utils
 from textnode import TextNode, TextType
-from utils import split_nodes_delimiter
 
 
 class TestSplitNodesDelimeter(unittest.TestCase):
     def test_code(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        new_nodes = utils.split_nodes_delimiter([node], "`", TextType.CODE)
         self.assertEqual(
             new_nodes,
             [
@@ -21,8 +21,8 @@ class TestSplitNodesDelimeter(unittest.TestCase):
         node = TextNode(
             "This is text with **bold text** and _italic text_", TextType.TEXT
         )
-        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
-        new_nodes = split_nodes_delimiter(new_nodes, "**", TextType.BOLD)
+        new_nodes = utils.split_nodes_delimiter([node], "_", TextType.ITALIC)
+        new_nodes = utils.split_nodes_delimiter(new_nodes, "**", TextType.BOLD)
         self.assertEqual(
             new_nodes,
             [
@@ -31,4 +31,38 @@ class TestSplitNodesDelimeter(unittest.TestCase):
                 TextNode(" and ", TextType.TEXT),
                 TextNode("italic text", TextType.ITALIC),
             ],
+        )
+
+
+class TestExtractMDImages(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = utils.extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_multiple_images(self):
+        matches = utils.extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual(
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+            matches,
+        )
+
+
+class TestExtractMDLinks(unittest.TestCase):
+    def test_extract_markdown_links(self):
+        matches = utils.extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+            matches,
         )
